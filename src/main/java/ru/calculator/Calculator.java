1ce9f70d1e920a46d66fbfc1fb2115db9a48201b
Rustam <rvalkaev@it-one.ru>
2023-08-21 22:26:35 +0400

    1.8 Вызов метода без указания. Доработал и сделал статический импорт метода

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,10 +1,12 @@
 package ru.calculator;
 
+import static ru.calculator.MathFunc.func1;
+
 public class Calculator {
     public static void main(String[] args) {
         MathFunc mathFunc = new MathFunc();
 
-        int result3 = mathFunc.func1(100);
+        int result3 = func1(100);
         System.out.println(result3);
     }
 }
