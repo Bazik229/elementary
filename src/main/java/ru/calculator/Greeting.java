a831f45f9b6c4f48a594b41532aa2bc88772a6b0
Rustam <rvalkaev@it-one.ru>
2023-08-22 23:05:10 +0400

    1.14 String

---
+++ src/main/java/ru/calculator/Greeting.java
@@ -0,0 +1,18 @@
+package ru.calculator;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2023;
+
+        System.out.println(idea);
+
+        idea = "But I am a newbie.";
+
+        System.out.println(idea);
+
+        idea += year;
+
+        System.out.println(idea);
+    }
+}
